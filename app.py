416573355a717c45c9a5d758477ac051da6e0a53
estrella272729 <estrella272729@gmail.com>
2025-10-16 10:26:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ if st.button('ON'):
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
-    ret= client1.publish("cmqtt_s", message)
+    ret= client1.publish("cmqtt_angies", message)
  
     #client1.subscribe("Sensores")
     
@@ -48,11 +48,11 @@ else:
 
 if st.button('OFF'):
     act1="OFF"
-    client1= paho.Client("GIT-HUB")                           
+    client1= paho.Client("angie")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)  
     message =json.dumps({"Act1":act1})
-    ret= client1.publish("cmqtt_s", message)
+    ret= client1.publish("cmqtt_angies", message)
   
     
 else:
@@ -62,11 +62,11 @@ values = st.slider('Selecciona el rango de valores',0.0, 100.0)
 st.write('Values:', values)
 
 if st.button('Enviar valor analógico'):
-    client1= paho.Client("GIT-HUB")                           
+    client1= paho.Client("angie")                           
     client1.on_publish = on_publish                          
     client1.connect(broker,port)   
     message =json.dumps({"Analog": float(values)})
-    ret= client1.publish("cmqtt_a", message)
+    ret= client1.publish("cmqtt_angiel", message)
     
  
 else:
